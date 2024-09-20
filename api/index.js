b7826cb4521b398615835f520add8eda4786a6d8
samyak <samyakmhrzn9841@gmail.com>
2024-09-20 15:39:47 +0545

    t2

--- api/index.js
+++ api/index.js
@@ -16,6 +16,7 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+export const maxDuration = 50;
 
 const app = express();
 dotenv.config();
